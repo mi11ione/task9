c07b68c4aaccaa28e562d626bf0224594f5a6dc1
mi11ione <i@mi11ion.io>
2024-12-23 19:22:36 +0300

    cooooool

--- task9/ContentView.swift
+++ task9/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var offset: CGSize = .zero
+    @State private var isDragging = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Color.red
+            .overlay { Color.yellow.frame(width: 250).clipShape(Circle()).blur(radius: 40) }
+            .overlay {
+                Color.black
+                    .overlay {
+                        Circle().blendMode(.destinationOut).frame(width: 140)
+                            .overlay {
+                                Circle().blendMode(.destinationOut).frame(width: 140).offset(offset)
+                                    .overlay {
+                                        Image(systemName: "cloud.sun.rain.fill")
+                                            .symbolRenderingMode(.hierarchical)
+                                            .resizable()
+                                            .frame(width: 80, height: 80)
+                                            .foregroundColor(.white)
+                                            .offset(offset)
+                                    }
+                                    .gesture(
+                                        DragGesture()
+                                            .onChanged { value in
+                                                isDragging = true
+                                                offset = value.translation
+                                            }
+                                            .onEnded { _ in
+                                                isDragging = false
+                                                withAnimation(.interpolatingSpring(stiffness: 100, damping: 15)) { offset = .zero }
+                                            }
+                                    )
+                            }
+                    }
+                    .compositingGroup()
+            }
+            .ignoresSafeArea()
     }
 }
 
