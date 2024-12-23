5acc55db6948223ec2dcecbb3db6b60bd22bc76f
mi11ione <i@mi11ion.io>
2024-12-23 20:16:24 +0300

    cooler

--- task9/ContentView.swift
+++ task9/ContentView.swift
@@ -14,34 +14,31 @@ struct ContentView: View {
     var body: some View {
         Color.red
             .overlay { Color.yellow.frame(width: 300).clipShape(Circle()).blur(radius: 40) }
-            .overlay {
-                Color.black
-                    .overlay {
-                        Circle().blendMode(.destinationOut).frame(width: 130)
-                            .overlay {
-                                Circle().blendMode(.destinationOut).frame(width: 130).offset(offset)
-                                    .overlay {
-                                        Image(systemName: "cloud.sun.rain.fill")
-                                            .symbolRenderingMode(.hierarchical)
-                                            .resizable()
-                                            .frame(width: 80, height: 80)
-                                            .foregroundColor(.white)
-                                            .offset(offset)
-                                    }
-                                    .gesture(
-                                        DragGesture()
-                                            .onChanged { value in
-                                                isDragging = true
-                                                offset = value.translation
-                                            }
-                                            .onEnded { _ in
-                                                isDragging = false
-                                                withAnimation(.interpolatingSpring(stiffness: 100, damping: 15)) { offset = .zero }
-                                            }
-                                    )
-                            }
+            .overlay { Color.black
+                .overlay { Circle().blendMode(.destinationOut).frame(width: 130)
+                    .overlay { Circle().blendMode(.destinationOut).frame(width: 130).offset(offset)
+                        .overlay {
+                            Image(systemName: "cloud.sun.rain.fill")
+                                .symbolRenderingMode(.hierarchical)
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                                .foregroundColor(.white)
+                                .offset(offset)
+                        }
+                        .gesture(
+                            DragGesture()
+                                .onChanged { value in
+                                    isDragging = true
+                                    offset = value.translation
+                                }
+                                .onEnded { _ in
+                                    isDragging = false
+                                    withAnimation(.interpolatingSpring(stiffness: 100, damping: 15)) { offset = .zero }
+                                }
+                        )
                     }
-                    .compositingGroup()
+                }
+                .compositingGroup()
             }
             .ignoresSafeArea()
     }
