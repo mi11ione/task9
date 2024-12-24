6839bb2467b76740d483a8da864f4129a92b8f56
mi11ione <i@mi11ion.io>
2024-12-24 04:26:45 +0300

    SWAG

--- task9/ContentView.swift
+++ task9/ContentView.swift
@@ -12,31 +12,74 @@ struct ContentView: View {
     @State private var isDragging = false
 
     var body: some View {
-        Color.black
-            .overlay { Circle().fill(.yellow).frame(width: 130)
-                .overlay { Circle().fill(.red).frame(width: 130).offset(offset)
-                    .overlay {
-                        Image(systemName: "cloud.sun.rain.fill")
-                            .symbolRenderingMode(.hierarchical)
-                            .resizable()
-                            .frame(width: 80, height: 80)
-                            .foregroundColor(.white)
-                            .offset(offset)
+        ZStack {
+            Color.red
+            Color.yellow
+                .clipShape(Circle())
+                .frame(width: 200, height: 200)
+                .blur(radius: 10)
+
+            ZStack {
+                Color.black
+
+                Canvas { context, size in
+                    context.addFilter(.alphaThreshold(min: 0.5, color: .yellow))
+                    context.addFilter(.blur(radius: 18))
+
+                    context.drawLayer { ctx in
+                        if let baseCircle = context.resolveSymbol(id: 0) {
+                            ctx.draw(baseCircle, in: CGRect(x: (size.width - 130) / 2, y: (size.height - 130) / 2, width: 130, height: 130))
+                        }
+
+                        if let dragCircle = context.resolveSymbol(id: 0) {
+                            let rect = CGRect(
+                                x: (size.width - 130) / 2 + offset.width,
+                                y: (size.height - 130) / 2 + offset.height,
+                                width: 130,
+                                height: 130
+                            )
+                            ctx.draw(dragCircle, in: rect)
+                        }
                     }
-                    .gesture(
-                        DragGesture()
-                            .onChanged { value in
-                                isDragging = true
-                                offset = value.translation
-                            }
-                            .onEnded { _ in
-                                isDragging = false
-                                withAnimation(.interpolatingSpring(stiffness: 100, damping: 15)) { offset = .zero }
-                            }
-                    )
+                } symbols: {
+                    Circle()
+                        .tag(0)
                 }
+
+                Circle()
+                    .fill(.black)
+                    .frame(width: 128)
+                    .blendMode(.destinationOut)
+
+                Circle()
+                    .fill(.black)
+                    .frame(width: 128)
+                    .offset(offset)
+                    .blendMode(.destinationOut)
             }
-            .ignoresSafeArea()
+            .compositingGroup()
+
+            Image(systemName: "cloud.sun.rain.fill")
+                .symbolRenderingMode(.hierarchical)
+                .resizable()
+                .frame(width: 80, height: 80)
+                .foregroundColor(.white)
+                .offset(offset)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            isDragging = true
+                            offset = value.translation
+                        }
+                        .onEnded { _ in
+                            isDragging = false
+                            withAnimation(.interpolatingSpring(stiffness: 100, damping: 15)) {
+                                offset = .zero
+                            }
+                        }
+                )
+        }
+        .ignoresSafeArea()
     }
 }
 
