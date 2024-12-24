2f8c6efec85404f1d180243394c81e4960bfbeb9
mi11ione <i@mi11ion.io>
2024-12-24 12:13:49 +0300

    cooler

--- task9/ContentView.swift
+++ task9/ContentView.swift
@@ -8,27 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var offset: CGSize = .zero
-    @State private var isDragging = false
+    @State private var offset = CGSize.zero
 
     var body: some View {
         ZStack {
             Color.red
-            Color.yellow
-                .clipShape(Circle())
-                .frame(width: 200, height: 200)
-                .blur(radius: 10)
+            Circle().fill(.yellow).frame(width: 200).blur(radius: 10)
 
             ZStack {
                 Color.black
 
-                Canvas { context, size in
-                    context.addFilter(.alphaThreshold(min: 0.5, color: .yellow))
-                    context.addFilter(.blur(radius: 18))
+                Canvas { swag, size in
+                    swag.addFilter(.alphaThreshold(min: 0.5, color: .yellow))
+                    swag.addFilter(.blur(radius: 18))
 
-                    context.drawLayer { swag in
-                        swag.fill(Circle().path(in: CGRect(x: (size.width - 130) / 2, y: (size.height - 130) / 2, width: 130, height: 130)), with: .color(.black))
-                        swag.fill(Circle().path(in: CGRect(x: (size.width - 130) / 2 + offset.width, y: (size.height - 130) / 2 + offset.height, width: 130, height: 130)), with: .color(.black))
+                    swag.drawLayer { layer in
+                        let circle = Circle().path(in: CGRect(origin: CGPoint(x: (size.width - 130) / 2, y: (size.height - 130) / 2), size: CGSize(width: 130, height: 130)))
+                        layer.fill(circle, with: .color(.black))
+                        layer.fill(circle.offsetBy(dx: offset.width, dy: offset.height), with: .color(.black))
                     }
                 }
 
@@ -53,16 +50,8 @@ struct ContentView: View {
                 .offset(offset)
                 .gesture(
                     DragGesture()
-                        .onChanged { value in
-                            isDragging = true
-                            offset = value.translation
-                        }
-                        .onEnded { _ in
-                            isDragging = false
-                            withAnimation(.interpolatingSpring(stiffness: 100, damping: 15)) {
-                                offset = .zero
-                            }
-                        }
+                        .onChanged { offset = $0.translation }
+                        .onEnded { _ in withAnimation(.interpolatingSpring(stiffness: 100, damping: 15)) { offset = .zero } }
                 )
         }
         .ignoresSafeArea()
